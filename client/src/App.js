7c84a14d0b9fdf4eee09ae4a62db5443df7d19d0
Hussein Razian <husseinrazian@outlook.com>
2020-10-06 23:00:05 -0600

    Created Add Inventory Component and Styled for Mobile

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import './App.css';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
-import Warehouse from '';
-import WarehouseDetails from './Components/WareHouseDetails/WareHouseDetails';
-import Inventory from '';
-import InventoryDetails from '';
-import Header from './Components/Header/Header'
+// import Warehouse from './Components/';
+import AddInventory from './Components/AddInventory/AddInventory'
+// import WarehouseDetails from './Components/WareHouseDetails/WareHouseDetails';
+// import Inventory from '';
+// import InventoryDetails from '';
+// import Header from './Components/Header/Header'
 
 class App extends React.Component {
   render() {
   return (
     <>
-      <BrowserRouter>
+    <AddInventory />
+      {/* <BrowserRouter>
         <Header />
-        <Switch>
-          <Redirect exact from = '/' to = '/warehouse'/>
-          <Route exact = '/warehouse' component {Warehouse}/>
-          <Route exact path = '/warehouse/:id' component = {WarehouseDetails}/>
+        <Switch> */}
+          {/* <Redirect exact from = '/' to = '/warehouse'/> */}
+          {/* <Route exact = '/warehouse' component {Warehouse}/> */}
+          {/* <Route exact path = '/warehouse/:id' component = {WarehouseDetails}/>
           <Route exact = '/inventory' component = {Inventory}/>
           <Route exact path = '/inventory/:id' component = {InventoryDetails}/>
         </Switch>
-      </BrowserRouter>
+      </BrowserRouter> */}
     </>
   );
   }
